05bfb9a782cd2201f3adade130c6bb2c6b45caa4
Jakub Sokolowski <jakub.sokolowski@nokia.com>
2019-06-15 19:23:18 +0200

    Add dockerfiles for server. Add script to download records from master database.

---
+++ mongo-seed/export-database.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+DATE_FROM_UTC=1530403200
+DATE_TO_UTC=1532908800
+HOST="54.93.39.245"
+USER="gd2"
+PASSWORD="javaee"
+mongoexport --host ${HOST} -u ${USER} -p ${PASSWORD} --db assets -c bittrex \
+            --query '{date : {$gt: 1530403200,$lt: 1532908800}}'\
+            --out bittrex.json --jsonArray
+
+mongoexport --host ${HOST} -u ${USER} -p ${PASSWORD} --db assets -c reddit \
+            --query '{created_utc : {$gt: 1530403200,$lt: 1532908800}}'\
+            --out reddit.json --jsonArray
