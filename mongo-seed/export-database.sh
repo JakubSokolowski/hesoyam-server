94eb7c8b70c2d622833bc6b2732a35c4c359c033
Jakub Sokolowski <jakub.sokolowski@nokia.com>
2019-06-15 21:58:32 +0200

    Add .jar and .json files

--- mongo-seed/export-database.sh
+++ mongo-seed/export-database.sh
@@ -7,9 +7,9 @@ HOST="54.93.39.245"
 USER="gd2"
 PASSWORD="javaee"
 mongoexport --host ${HOST} -u ${USER} -p ${PASSWORD} --db assets -c bittrex \
-            --query '{date : {$gt: 1530403200,$lt: 1532908800}}'\
+            --query '{date : {$gt: 1530856800,$lt: 1531213200}}'\
             --out bittrex.json --jsonArray
 
 mongoexport --host ${HOST} -u ${USER} -p ${PASSWORD} --db assets -c reddit \
-            --query '{created_utc : {$gt: 1530403200,$lt: 1532908800}}'\
+            --query '{created_utc : {$gt: 1530856800,$lt: 1531213200}}'\
             --out reddit.json --jsonArray
